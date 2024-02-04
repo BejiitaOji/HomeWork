0be4ae969d33d1a5a9d2759e804dbbb2dd5d40b7
Ilja <97607971+BejiitaOji@users.noreply.github.com>
2024-02-04 14:08:02 +0300

    corrected comments

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 import java.util.HashMap;
 
 public class Main {
-    public static void main(String[] args) throws CustomCheckedException {
+    public static void main(String[] args) {
         CustomHashMap studentList = new CustomHashMap("Sergey", 1, "Dmitriy", 2, "Egor", 3);
 
         studentList.put("Sergey", 4);
 
         studentList.put("Dmitriy", 2);
+
+        System.out.println(studentList.get("Egor"));
 
         System.out.println(studentList);
 
